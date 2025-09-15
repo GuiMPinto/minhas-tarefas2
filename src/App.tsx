56d05394cfca41ffb63d0b303d9696111b760552
PintoJR <guimoraespinto@gmail.com>
2025-09-15 17:18:16 -0300

    P5-2_32-6

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
-import BarraLateral from './containers/BarraLateral'
-import ListaTarefas from './containers/ListaTarefas'
+/* CREATEBROWSERROUTER = Lugar onde vai ser configurada as páginas e o roteamento da aplicação */
+/* ROUTERPROVIDER = O componente que vai gerenciar a aplicação */
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import EstiloGlobal, { Container } from './styles'
 import { Provider } from 'react-redux'
 import store from './store'
+import Home from './pages/Home'
+
+const rotas = createBrowserRouter([
+  {
+    path: '/' /* propriedade que indiica um caminho. Ao encontrar o '/' algum elemento será renderizado */,
+    element: <Home /> /* Elemento a ser renderizado */
+  },
+  {
+    path: '/novo',
+    element: <h1>Nova tarefa</h1>
+  }
+])
 
 function App() {
   return (
     <Provider store={store}>
       <EstiloGlobal />
       <Container>
-        <BarraLateral />
-        <ListaTarefas />
+        <RouterProvider router={rotas} />
       </Container>
     </Provider>
   )
